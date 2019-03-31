119ec1724fd765b1c4148a70e10cea8af306dcbb
ghostwalkr <46386104+ghostwalkr@users.noreply.github.com>
2019-03-31 20:19:57 +0000

    Banner grabber update
    Updated to now use command line arguments for host and port options. Still only really does HTTP banner grabs though.

--- banner.py
+++ banner.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 # Python banner grabber
 import socket
-host = "192.168.1.68"
-port = 80
-request = "HEAD / HTTP/1.1\r\n\r\n"
+import sys
+
+# Command line arguments are host (1) and port (2)
+host = sys.argv[1]
+port = int(sys.argv[2])
+http_request = "GET / HTTP/1.1\r\n\r\n" #The request for HTTP port 80
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("socket made")
+print "socket made"
 s.connect((host, port))
 print "connected to " + host
-s.sendall(request)
+s.sendall(http_request)
 print "request sent"
 banner = s.recv(4096)
 print(banner)
 s.close()
+print "All done boss"
